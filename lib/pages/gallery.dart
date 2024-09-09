54a5d8f400b5ee3fdb6f9c88f228db3ebff92dcb
Bruno Silva Freire <brunorbsf@gmail.com>
2024-09-09 16:15:16 -0300

    fix: Use a listenable builder instead of a riverpod provider to avoid double dispose of text controller

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -39,146 +39,148 @@ class _EasterEggGalleryState extends ConsumerState<EasterEggGallery> {
     var filtered = ref.watch(_gameFilterProvider);
     var games = ref.watch(gameManifestProvider);
 
-    ref.watch(ChangeNotifierProvider(
-      (ref) => _searchController,
-    ));
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxWidth: 256,
-          ),
-          child: ContainerCard.leftSideContainer(
-            children: [
-              FloatingActionButton.extended(
-                elevation: 0,
-                onPressed: () {
-                  showModal(
-                    context: context,
-                    useRootNavigator: true,
-                    builder: (context) {
-                      return const Center(
-                        child: CreateGuideForm(),
-                      );
-                    },
-                  );
-                },
-                icon: Icon(MdiIcons.bookPlus),
-                label: const Text("Create new guide"),
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          child: CustomScrollView(
-            shrinkWrap: true,
-            slivers: [
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 16,
-                ),
+    return ListenableBuilder(
+      listenable: _searchController,
+      builder: (context, _) {
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 256,
               ),
-              SliverAppBar(
-                primary: true,
-                flexibleSpace: SearchBar(
-                  controller: _searchController,
-                  leading: Icon(MdiIcons.bookSearch),
-                  hintText: "Search by map, game or easter egg name",
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Wrap(
-                    spacing: 8,
-                    children: [
-                      ...games.maybeWhen(
-                        orElse: () {
-                          return const <Widget>[];
-                        },
-                        data: (data) {
-                          return [
-                            for (var MapEntry(key: edition, value: game)
-                                in data.entries)
-                              FilterChip(
-                                avatar: Image.asset(game.thumbnail),
-                                label: Text(game.title),
-                                selected: filtered.contains(edition),
-                                onSelected: (bool value) {
-                                  var newSet = {...filtered};
-                                  if (value) {
-                                    newSet.add(edition);
-                                  } else {
-                                    newSet.remove(edition);
-                                  }
-                                  ref.read(_gameFilterProvider.notifier).state =
-                                      newSet;
-                                },
-                              ),
-                          ];
-                        },
-                        loading: () {
-                          return const <Widget>[
-                            CircularProgressIndicator(),
-                          ];
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: ListTile(
-                  title: const Text("Bundled Guides"),
-                  subtitle: const Text(
-                    "Guides that are bundled with the app. These guides are curated by the developers.",
-                  ),
-                  leading: Icon(MdiIcons.fileCertificate),
-                ),
-              ),
-              SliverEasterEggGrid(
-                easterEggs: bundledEasterEggs,
-                predicate: shouldShow,
-              ),
-              SliverToBoxAdapter(
-                child: ListTile(
-                  title: const Text("Your Guides"),
-                  subtitle: const Text(
-                    "Guides you have created. These are only stored locally in your device or on your browser. So be careful about losing your data.",
-                  ),
-                  leading: Icon(MdiIcons.fileAccount),
-                ),
-              ),
-              SliverEasterEggGrid(
-                easterEggs: localEasterEggs,
-                predicate: shouldShow,
-                badgeBuilder: (easterEgg) {
-                  return IconButton.filledTonal(
-                    icon: Icon(MdiIcons.trashCan),
-                    onPressed: () async {
-                      var shouldDelete = await showModal(
+              child: ContainerCard.leftSideContainer(
+                children: [
+                  FloatingActionButton.extended(
+                    elevation: 0,
+                    onPressed: () {
+                      showModal(
                         context: context,
-                        configuration: const FadeScaleTransitionConfiguration(
-                          barrierDismissible: false,
-                        ),
+                        useRootNavigator: true,
                         builder: (context) {
-                          return const ConfirmationDialog();
+                          return const Center(
+                            child: CreateGuideForm(),
+                          );
                         },
                       );
-                      if (shouldDelete == true) {
-                        ref
-                            .read(localEasterEggRegistryProvider.notifier)
-                            .deleteEasterEgg(easterEgg);
-                      }
                     },
-                  );
-                },
+                    icon: Icon(MdiIcons.bookPlus),
+                    label: const Text("Create new guide"),
+                  )
+                ],
               ),
-            ],
-          ),
-        )
-      ],
+            ),
+            Expanded(
+              child: CustomScrollView(
+                shrinkWrap: true,
+                slivers: [
+                  const SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 16,
+                    ),
+                  ),
+                  SliverAppBar(
+                    primary: true,
+                    flexibleSpace: SearchBar(
+                      controller: _searchController,
+                      leading: Icon(MdiIcons.bookSearch),
+                      hintText: "Search by map, game or easter egg name",
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Wrap(
+                        spacing: 8,
+                        children: [
+                          ...games.maybeWhen(
+                            orElse: () {
+                              return const <Widget>[];
+                            },
+                            data: (data) {
+                              return [
+                                for (var MapEntry(key: edition, value: game)
+                                    in data.entries)
+                                  FilterChip(
+                                    avatar: Image.asset(game.thumbnail),
+                                    label: Text(game.title),
+                                    selected: filtered.contains(edition),
+                                    onSelected: (bool value) {
+                                      var newSet = {...filtered};
+                                      if (value) {
+                                        newSet.add(edition);
+                                      } else {
+                                        newSet.remove(edition);
+                                      }
+                                      ref.read(_gameFilterProvider.notifier).state =
+                                          newSet;
+                                    },
+                                  ),
+                              ];
+                            },
+                            loading: () {
+                              return const <Widget>[
+                                CircularProgressIndicator(),
+                              ];
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: ListTile(
+                      title: const Text("Bundled Guides"),
+                      subtitle: const Text(
+                        "Guides that are bundled with the app. These guides are curated by the developers.",
+                      ),
+                      leading: Icon(MdiIcons.fileCertificate),
+                    ),
+                  ),
+                  SliverEasterEggGrid(
+                    easterEggs: bundledEasterEggs,
+                    predicate: shouldShow,
+                  ),
+                  SliverToBoxAdapter(
+                    child: ListTile(
+                      title: const Text("Your Guides"),
+                      subtitle: const Text(
+                        "Guides you have created. These are only stored locally in your device or on your browser. So be careful about losing your data.",
+                      ),
+                      leading: Icon(MdiIcons.fileAccount),
+                    ),
+                  ),
+                  SliverEasterEggGrid(
+                    easterEggs: localEasterEggs,
+                    predicate: shouldShow,
+                    badgeBuilder: (easterEgg) {
+                      return IconButton.filledTonal(
+                        icon: Icon(MdiIcons.trashCan),
+                        onPressed: () async {
+                          var shouldDelete = await showModal(
+                            context: context,
+                            configuration: const FadeScaleTransitionConfiguration(
+                              barrierDismissible: false,
+                            ),
+                            builder: (context) {
+                              return const ConfirmationDialog();
+                            },
+                          );
+                          if (shouldDelete == true) {
+                            ref
+                                .read(localEasterEggRegistryProvider.notifier)
+                                .deleteEasterEgg(easterEgg);
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        );
+      }
     );
   }
 
